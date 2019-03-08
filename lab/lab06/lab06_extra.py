387b8fcbe3e8fa80d367b3c17491afd318848a4f
EpsilonHF <epsilonhf@outlook.com>
2019-03-08 23:38:34 +0800

    add hw06 lab06

---
+++ lab/lab06/lab06_extra.py
@@ -0,0 +1,32 @@
+""" Optional problems for Lab 6 """
+
+## Nonlocal practice
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    "*** YOUR CODE HERE ***"
+    i = 0
+    def circle():
+        nonlocal i
+        if i == len(snacks):
+            i = 0
+        res = snacks[i]
+        i += 1
+        return res
+
+    return circle
